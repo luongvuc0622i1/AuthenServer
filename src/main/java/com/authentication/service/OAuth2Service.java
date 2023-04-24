39ab3e6f26c51cb95e9b195c4e0dc03ef0588812
luongvuc0622i1 <vuhienluong.td151218@gmail.com>
2023-04-24 11:42:48 +0700

    second

---
+++ src/main/java/com/authentication/service/OAuth2Service.java
@@ -0,0 +1,7 @@
+package com.authentication.service;
+
+import com.authentication.model.AccessToken;
+
+public interface OAuth2Service {
+    AccessToken getAccessToken(String clientId, String clientSecret, String redirectUri, String code, String state);
+}
